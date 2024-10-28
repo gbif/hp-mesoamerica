4286e1212eb41440734c1c6261709b3cb6f2ae58
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-10-28 18:38:59 +0100

    show catalogues

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -11,6 +11,7 @@ var siteConfig = {
     alwaysUseHrefs: true, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
     enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey'],
   },
+  availableCatalogues: ['OCCURRENCE', 'COLLECTION', 'INSTITUTION'],
   occurrence: {
     mapSettings: {
       lat: 17.816627,
