f211e164c9f262193303b889128f7a7cb0dcda05
KiwilEstafas <paulitakvist@hotmail.com>
2024-12-09 11:21:06 -0600

    create dataset page

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -9,9 +9,9 @@ var siteConfig = {
   version: 2,
   routes: {
     alwaysUseHrefs: true, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
-    enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey'],
+    enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey',"datasetKey","datasetSearch"],
   },
-  availableCatalogues: ['OCCURRENCE', 'COLLECTION', 'INSTITUTION'],
+  availableCatalogues: ['OCCURRENCE', 'COLLECTION', 'INSTITUTION',"DATASET"],
   occurrence: {
     mapSettings: {
       lat: 17.816627,
@@ -58,6 +58,11 @@ var siteConfig = {
       lng: -98.35669392677512,
       zoom: 4.809933245,
     },
+  },
+  dataset: {
+    rootFilter: {
+      publishingCountry: countryCodes
+    }
   },
 };
 
---
+++ dataset.md
@@ -0,0 +1,7 @@
+---
+permalink: dataset/search
+layout: dataset-search
+title: Dataset
+description: Explore our datasets
+lang-ref: dataset
+---
